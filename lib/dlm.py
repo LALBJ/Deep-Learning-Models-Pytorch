8f9b2fa8b46bfbcdc10f6d31379dce092bd8a0fd
LALBJ <905976755@qq.com>
2022-10-21 17:27:33 +0800

    feat: add seq2seq model

--- lib/dlm.py
+++ lib/dlm.py
@@ -12,6 +12,8 @@ import sys
 import hashlib
 import os
 import requests
+import zipfile
+import tarfile
 import re
 import collections
 import inspect
@@ -240,6 +242,77 @@ class Trainer(dlm.HyperParameters):
             for param in params:
                 param.grad[:] *= grad_clip_val / norm
 
+class MTFraEng(dlm.DataModule):
+    """Defined in :numref:`sec_machine_translation`"""
+    def _download(self):
+        dlm.extract(dlm.download(
+            dlm.DATA_URL+'fra-eng.zip', self.root,
+            '94646ad1522d915e7b0f9296181140edcf86a4f5'))
+        with open(self.root + '/fra-eng/fra.txt', encoding='utf-8') as f:
+            return f.read()
+
+    def _preprocess(self, text):
+        """Defined in :numref:`sec_machine_translation`"""
+        # Replace non-breaking space with space
+        text = text.replace('\u202f', ' ').replace('\xa0', ' ')
+        # Insert space between words and punctuation marks
+        no_space = lambda char, prev_char: char in ',.!?' and prev_char != ' '
+        out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
+               for i, char in enumerate(text.lower())]
+        return ''.join(out)
+
+    def _tokenize(self, text, max_examples=None):
+        """Defined in :numref:`sec_machine_translation`"""
+        src, tgt = [], []
+        for i, line in enumerate(text.split('\n')):
+            if max_examples and i > max_examples: break
+            parts = line.split('\t')
+            if len(parts) == 2:
+                # Skip empty tokens
+                src.append([t for t in f'{parts[0]} <eos>'.split(' ') if t])
+                tgt.append([t for t in f'{parts[1]} <eos>'.split(' ') if t])
+        return src, tgt
+
+    def __init__(self, batch_size, num_steps=9, num_train=512, num_val=128):
+        """Defined in :numref:`sec_machine_translation`"""
+        super(MTFraEng, self).__init__()
+        self.save_hyperparameters()
+        self.arrays, self.src_vocab, self.tgt_vocab = self._build_arrays(
+            self._download())
+    
+    def _build_arrays(self, raw_text, src_vocab=None, tgt_vocab=None):
+        """Defined in :numref:`sec_machine_translation`"""
+        def _build_array(sentences, vocab, is_tgt=False):
+            pad_or_trim = lambda seq, t: (
+                seq[:t] if len(seq) > t else seq + ['<pad>'] * (t - len(seq)))
+            sentences = [pad_or_trim(s, self.num_steps) for s in sentences]
+            if is_tgt:
+                sentences = [['<bos>'] + s for s in sentences]
+            if vocab is None:
+                vocab = dlm.Vocab(sentences, min_freq=2)
+            array = dlm.tensor([vocab[s] for s in sentences])
+            valid_len = dlm.reduce_sum(
+                dlm.astype(array != vocab['<pad>'], dlm.int32), 1)
+            return array, vocab, valid_len
+        src, tgt = self._tokenize(self._preprocess(raw_text),
+                                  self.num_train + self.num_val)
+        src_array, src_vocab, src_valid_len = _build_array(src, src_vocab)
+        tgt_array, tgt_vocab, _ = _build_array(tgt, tgt_vocab, True)
+        return ((src_array, tgt_array[:,:-1], src_valid_len, tgt_array[:,1:]),
+                src_vocab, tgt_vocab)
+
+    def get_dataloader(self, train):
+        """Defined in :numref:`subsec_loading-seq-fixed-len`"""
+        idx = slice(0, self.num_train) if train else slice(self.num_train, None)
+        return self.get_tensorloader(self.arrays, train, idx)
+
+    def build(self, src_sentences, tgt_sentences):
+        """Defined in :numref:`subsec_loading-seq-fixed-len`"""
+        raw_text = '\n'.join([src + '\t' + tgt for src, tgt in zip(
+            src_sentences, tgt_sentences)])
+        arrays, _, _ = self._build_arrays(
+            raw_text, self.src_vocab, self.tgt_vocab)
+        return arrays
 
 class TimeMachine(dlm.DataModule):
     def _download(self):
@@ -274,7 +347,66 @@ class TimeMachine(dlm.DataModule):
         idx = slice(0, self.num_train) if train else slice(
             self.num_train, self.num_train + self.num_val)
         return self.get_tensorloader([self.X, self.Y], train, idx)
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, X, *args):
+        raise NotImplementedError
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+class EncoderDecoder(dlm.Classifier):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
     
+    def forward(self, enc_X, dec_X, *args):
+        enc_outputs = self.encoder(enc_X, *args)
+        dec_state = self.decoder.init_state(enc_outputs, *args)
+
+        return self.decoder(dec_X, dec_state)[0]
+
+    def predict_step(self, batch, device, num_steps,
+                     save_attention_weights=False):
+        batch = [dlm.to(a, device) for a in batch]
+        src, tgt, src_valid_len, _ = batch
+        enc_outputs = self.encoder(src, src_valid_len)
+        dec_state = self.decoder.init_state(enc_outputs, src_valid_len)
+        outputs, attention_weights = [dlm.expand_dims(tgt[:,0], 1), ], []
+        for _ in range(num_steps):
+            Y, dec_state = self.decoder(outputs[-1], dec_state)
+            outputs.append(dlm.argmax(Y, 2))
+            # Save attention weights (to be covered later)
+            if save_attention_weights:
+                attention_weights.append(self.decoder.attention_weights)
+        return dlm.concat(outputs[1:], 1), attention_weights
+    
+class RNN(dlm.Module):
+    def __init__(self, num_inputs, num_hiddens):
+        super().__init__()
+        self.save_hyperparameters()
+        self.rnn = nn.RNN(num_inputs, num_hiddens)
+
+    def forward(self, inputs, H=None):
+        return self.rnn(inputs, H)
+class GRU(dlm.RNN):
+    def __init__(self, num_inputs, num_hiddens, num_layers, dropout=0):
+        dlm.Module.__init__(self)
+        self.save_hyperparameters()
+        self.rnn = nn.GRU(num_inputs, num_hiddens, num_layers,
+                          dropout=dropout)
+
 class RNNScratch(dlm.Module):
     def __init__(self, num_inputs, num_hiddens, sigma=0.01):
         super().__init__()
@@ -405,6 +537,18 @@ def download(url, folder='../data', sha1_hash=None):
     with open(fname, 'wb') as f:
         f.write(r.content)
     return fname
+
+def extract(filename, folder=None):
+    base_dir = os.path.dirname(filename)
+    _, ext = os.path.splitext(filename)
+    assert ext in ('.zip', '.tar', '.gz'), 'Only support zip/tar files.'
+    if ext == '.zip':
+        fp = zipfile.ZipFile(filename, 'r')
+    else:
+        fp = tarfile.open(filename, 'r')
+    if folder is None:
+        folder = base_dir
+    fp.extractall(folder)
 
 def cpu():
     return torch.device('cpu')
